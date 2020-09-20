7b4553b313bb727f1ec831fe00503fd0c8252d39
gregbabbert <60162794+gregbabbert@users.noreply.github.com>
2020-09-20 00:33:28 -0400

    Questions up to 2.88

--- Book.java
+++ Book.java
@@ -11,18 +11,40 @@ class Book
      // The fields.
     private String author;
     private String title;
+    //For 2.85
+    private int pages;
+    //For 2.88
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
     }
 
     // Add the methods here ...
+    /**
+     * Sets the reference number. This is for 2.88
+     */
+    public void setRefNumber(String ref) 
+    {
+        refNumber = ref;
+    }
+    
+    /**
+     * Gets the reference number. This is for 2.88
+     */
+    public String getRefNumber() 
+    {
+        return refNumber;
+    }
+    
     /**
      * Provide access to the string holding the author's name
      * @return author
@@ -41,8 +63,38 @@ class Book
         return title;
     }
     
+        /**
+     * Provide access to the int holding the book's pages
+     * @return pages
+     */
+    public int getPages()
+    {
+        return pages;
+    }
     
     
+    /**
+     * Prints author. This is for 2.84
+     */
+    public void printAuthor() 
+    {
+        System.out.println("The author of this book is: " + author);
+    }
     
+    /**
+     * Prints title. This is for 2.84
+     */
+    public void printTitle() 
+    {
+        System.out.println("The title of this book is" + title);
+    }
+    
+    /**
+     * Prints the book's details. This is for 2.87
+     */
+    public void printDetails() 
+    {
+        System.out.println("Title: " + title + ", Author: " + author + ", Pages: " + pages);
+    }
     
 }
